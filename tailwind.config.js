30668303be74ce9ba9071096c509260c6ac652d7
Kyulol <sivan.jeremie@gmail.com>
2021-04-30 20:21:45 +0200

    init template
    install react
    install tailwind
    install sass

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito']
+      },
+      colors: {
+        almostBlack: '#1a1a1a',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      fontWeight: ['hover', 'focus',],
+      borderWidth: ['hover', 'focus', 'active'],
+      borderColor: ['hover', 'focus', 'active'],
+      backgroundClip: ['hover', 'focus'],
+      backgroundImage: ['hover', 'focus'],
+      backgroundColor: ['active', 'checked'],
+      padding: ['hover', 'focus'],
+      margin: ['hover', 'focus'],
+    },
+  },
+  plugins: [],
+}
